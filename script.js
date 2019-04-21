ff33e5b9549ee1cb64d0f2768cca8346abb8e092
Denys Zhorov <2013dvl@gmail.com>
2019-04-21 12:28:29 +0200

    Creat POST request form

---
+++ script.js
@@ -0,0 +1,48 @@
+let message = {
+        loading: "Loading...",
+        success: "Thank! Soon we will contact you!",
+        failure: "Something went wrong..."
+    };
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    function postServerForm(event, targetEvent) {
+        event.preventDefault();
+        
+        targetEvent.appendChild(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(targetEvent);
+        
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        
+        let json = JSON.stringify(obj);
+        
+        request.send(json);
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    }
+
+    form.addEventListener('submit', function (event) {
+        let eventForm = event,
+            target = event.target;
+        postServerForm(eventForm, target);
+    });
